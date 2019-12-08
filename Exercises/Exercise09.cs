2b300ad4440ed28f0f25a2af8fa5c7bb87f4d74c
mbenjamin <mbenjamin@instechnologies.net>
2019-12-08 12:28:26 -0500

    Set up class for Exercise 9.

---
+++ Exercises/Exercise09.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode.Dependencies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Exercises
+{
+    public class Exercise09 : Exercise
+    {
+        public override string FileName => "Exercise09";
+
+        public override int ProblemOne()
+        {
+            return 0;
+        }
+
+        public override int ProblemTwo()
+        {
+            return 0;
+        }
+    }
+}
